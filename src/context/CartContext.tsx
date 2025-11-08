70cc3c8aa1a7a161a3ddc671b4531cfa1d839616
YasinDehfuli <ydehfuli@gmail.com>
2025-11-08 17:28:10 +0330

    feat(toast): added toast service to cart changes

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -2,11 +2,13 @@
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { Product, CartItem, CartContextType } from '@/types/productTypes';
+import { useToast } from '@/context/ToastContext';
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
 export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [cart, setCart] = useState<CartItem[]>([]);
+    const toast = useToast();
 
     useEffect(() => {
         const savedCart = localStorage.getItem('cart');
@@ -20,20 +22,28 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
     }, [cart]);
 
     const addToCart = (product: Product) => {
-        setCart((prevCart) => {
-            const existingItem = prevCart.find((item) => item.id === product.id);
-            if (existingItem) {
-                return prevCart.map((item) =>
+        const existingItem = cart.find((item) => item.id === product.id);
+
+        if (existingItem) {
+            setCart((prevCart) =>
+                prevCart.map((item) =>
                     item.id === product.id
                         ? { ...item, quantity: item.quantity + 1 }
                         : item
-                );
-            }
-            return [...prevCart, { ...product, quantity: 1 }];
-        });
+                )
+            );
+            toast.success(`Added one more {${product.title}} to cart!`);
+        } else {
+            setCart((prevCart) => [...prevCart, { ...product, quantity: 1 }]);
+            toast.success(`{${product.title}} added to cart!`);
+        }
     };
 
     const removeFromCart = (productId: number) => {
+        const item = cart.find((item) => item.id === productId);
+        if (item) {
+            toast.info(`{${item.title}} removed from cart`);
+        }
         setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
     };
 
@@ -50,6 +60,9 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
     };
 
     const clearCart = () => {
+        if (cart.length > 0) {
+            toast.success('clear all card');
+        }
         setCart([]);
     };
 
